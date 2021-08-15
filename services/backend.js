371c9c4ce0503ade8b190e281d58b854b4cf41fa
DIDIX <grouteur@gmail.com>
2021-08-15 22:39:08 +0200

    modif backend pour appel dofusroom

--- services/backend.js
+++ services/backend.js
@@ -14,14 +14,15 @@
  *    limitations under the License.
  */
 
-const fs = require("fs");
+// const fs = require("fs");
 const Hapi = require("hapi");
-const path = require("path");
+// const path = require("path");
 const Boom = require("boom");
-const color = require("color");
+// const color = require("color");
 const ext = require("commander");
 const jsonwebtoken = require("jsonwebtoken");
-const request = require("request");
+const puppeteer = require("puppeteer");
+const cloudscraper = require("cloudscraper");
 
 // The developer rig uses self-signed certificates.  Node doesn't accept them
 // by default.  Do not use this in production.
@@ -32,25 +33,25 @@ const verboseLogging = true;
 const verboseLog = verboseLogging ? console.log.bind(console) : () => {};
 
 // Service state variables
-const initialColor = color("#6441A4"); // super important; bleedPurple, etc.
+// const initialColor = color("#6441A4"); // super important; bleedPurple, etc.
 const serverTokenDurationSec = 30; // our tokens for pubsub expire after 30 seconds
-const userCooldownMs = 1000; // maximum input rate per user to prevent bot abuse
+// const userCooldownMs = 1000; // maximum input rate per user to prevent bot abuse
 const userCooldownClearIntervalMs = 60000; // interval to reset our tracking object
 const channelCooldownMs = 1000; // maximum broadcast rate per channel
 const bearerPrefix = "Bearer "; // HTTP authorization headers have this prefix
-const colorWheelRotation = 30;
-const channelColors = {};
+// const colorWheelRotation = 30;
+let htmlResponse = "";
 const channelCooldowns = {}; // rate limit compliance
-let userCooldowns = {}; // spam prevention
+// let userCooldowns = {}; // spam prevention
 
 const STRINGS = {
-  secretEnv: usingValue("secret"),
-  clientIdEnv: usingValue("client-id"),
-  ownerIdEnv: usingValue("owner-id"),
+  // secretEnv: usingValue("secret"),
+  // clientIdEnv: usingValue("client-id"),
+  // ownerIdEnv: usingValue("owner-id"),
   serverStarted: "Server running at %s",
-  secretMissing: missingValue("secret", "EXT_SECRET"),
-  clientIdMissing: missingValue("client ID", "EXT_CLIENT_ID"),
-  ownerIdMissing: missingValue("owner ID", "EXT_OWNER_ID"),
+  // secretMissing: missingValue("secret", "EXT_SECRET"),
+  // clientIdMissing: missingValue("client ID", "EXT_CLIENT_ID"),
+  // ownerIdMissing: missingValue("owner ID", "EXT_OWNER_ID"),
   messageSendError: "Error sending message to channel %s: %s",
   pubsubResponse: "Message to c:%s returned %s",
   cyclingColor: "Cycling color for c:%s on behalf of u:%s",
@@ -81,34 +82,34 @@ const serverOptions = {
     },
   },
 };
-const serverPathRoot = path.resolve(__dirname, "..", "conf", "server");
-if (fs.existsSync(serverPathRoot + ".crt") && fs.existsSync(serverPathRoot + ".key")) {
-  serverOptions.tls = {
-    // If you need a certificate, execute "npm run cert".
-    cert: fs.readFileSync(serverPathRoot + ".crt"),
-    key: fs.readFileSync(serverPathRoot + ".key"),
-  };
-}
+// const serverPathRoot = path.resolve(__dirname, "..", "conf", "server");
+// if (fs.existsSync(serverPathRoot + ".crt") && fs.existsSync(serverPathRoot + ".key")) {
+//   serverOptions.tls = {
+//     // If you need a certificate, execute "npm run cert".
+//     cert: fs.readFileSync(serverPathRoot + ".crt"),
+//     key: fs.readFileSync(serverPathRoot + ".key"),
+//   };
+// }
 const server = new Hapi.Server(serverOptions);
 
 (async () => {
   // Handle a viewer request to cycle the color.
-  server.route({
-    method: "POST",
-    path: "/color/cycle",
-    handler: colorCycleHandler,
-  });
+  // server.route({
+  //   method: "POST",
+  //   path: "/color/cycle",
+  //   handler: colorCycleHandler,
+  // });
 
   // Handle a new viewer requesting the color.
   server.route({
     method: "GET",
-    path: "/color/query",
-    handler: colorQueryHandler,
+    path: "/stuff",
+    handler: stuffQueryHandler,
   });
 
   // Start the server.
   await server.start();
-  console.log(STRINGS.serverStarted, server.info.uri);
+  console.log("Serveur OK");
 
   // Periodically clear cool-down tracking to prevent unbounded growth due to
   // per-session logged-out user tokens.
@@ -117,14 +118,14 @@ const server = new Hapi.Server(serverOptions);
   }, userCooldownClearIntervalMs);
 })();
 
-function usingValue(name) {
-  return `Using environment variable for ${name}`;
-}
+// function usingValue(name) {
+//   return `Using environment variable for ${name}`;
+// }
 
-function missingValue(name, variable) {
-  const option = name.charAt(0);
-  return `Extension ${name} required.\nUse argument "-${option} <${name}>" or environment variable "${variable}".`;
-}
+// function missingValue(name, variable) {
+//   const option = name.charAt(0);
+//   return `Extension ${name} required.\nUse argument "-${option} <${name}>" or environment variable "${variable}".`;
+// }
 
 // Get options from the command line or the environment.
 function getOption(optionName, environmentName) {
@@ -155,115 +156,193 @@ function verifyAndDecode(header) {
   throw Boom.unauthorized(STRINGS.invalidAuthHeader);
 }
 
-function colorCycleHandler(req) {
+// function colorCycleHandler(req) {
+//   // Verify all requests.
+//   const payload = verifyAndDecode(req.headers.authorization);
+//   const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
+
+//   // Store the color for the channel.
+//   let currentColor = channelColors[channelId] || initialColor;
+
+//   // Bot abuse prevention:  don't allow a user to spam the button.
+//   if (userIsInCooldown(opaqueUserId)) {
+//     throw Boom.tooManyRequests(STRINGS.cooldown);
+//   }
+
+//   // Rotate the color as if on a color wheel.
+//   verboseLog(STRINGS.cyclingColor, channelId, opaqueUserId);
+//   currentColor = color(currentColor).rotate(colorWheelRotation).hex();
+
+//   // Save the new color for the channel.
+//   channelColors[channelId] = currentColor;
+
+//   // Broadcast the color change to all other extension instances on this channel.
+//   attemptColorBroadcast(channelId);
+
+//   return currentColor;
+// }
+
+// function delay(time) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, time);
+//   });
+// }
+
+function stuffQueryHandler(req) {
   // Verify all requests.
   const payload = verifyAndDecode(req.headers.authorization);
   const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
 
-  // Store the color for the channel.
-  let currentColor = channelColors[channelId] || initialColor;
+  if (typeof req.query.mode !== "undefined" && typeof req.query.url !== "undefined") {
+    const config = { mode: req.query.mode, link: req.query.url };
 
-  // Bot abuse prevention:  don't allow a user to spam the button.
-  if (userIsInCooldown(opaqueUserId)) {
-    throw Boom.tooManyRequests(STRINGS.cooldown);
-  }
-
-  // Rotate the color as if on a color wheel.
-  verboseLog(STRINGS.cyclingColor, channelId, opaqueUserId);
-  currentColor = color(currentColor).rotate(colorWheelRotation).hex();
-
-  // Save the new color for the channel.
-  channelColors[channelId] = currentColor;
-
-  // Broadcast the color change to all other extension instances on this channel.
-  attemptColorBroadcast(channelId);
-
-  return currentColor;
-}
-
-function colorQueryHandler(req) {
-  // Verify all requests.
-  const payload = verifyAndDecode(req.headers.authorization);
-  debugger;
-  // Get the color for the channel from the payload and return it.
-  const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
-  const currentColor = color(channelColors[channelId] || initialColor).hex();
-  verboseLog(STRINGS.sendColor, currentColor, opaqueUserId);
-  return currentColor;
-}
-
-function attemptColorBroadcast(channelId) {
-  // Check the cool-down to determine if it's okay to send now.
-  const now = Date.now();
-  const cooldown = channelCooldowns[channelId];
-  if (!cooldown || cooldown.time < now) {
-    // It is.
-    sendColorBroadcast(channelId);
-    channelCooldowns[channelId] = { time: now + channelCooldownMs };
-  } else if (!cooldown.trigger) {
-    // It isn't; schedule a delayed broadcast if we haven't already done so.
-    cooldown.trigger = setTimeout(sendColorBroadcast, now - cooldown.time, channelId);
-  }
-}
-
-function sendColorBroadcast(channelId) {
-  // Set the HTTP headers required by the Twitch API.
-  const headers = {
-    "Client-ID": clientId,
-    "Content-Type": "application/json",
-    Authorization: bearerPrefix + makeServerToken(channelId),
-  };
-
-  // Create the POST body for the Twitch API request.
-  const currentColor = color(channelColors[channelId] || initialColor).hex();
-  const body = JSON.stringify({
-    content_type: "application/json",
-    message: currentColor,
-    targets: ["broadcast"],
-  });
-
-  // Send the broadcast request to the Twitch API.
-  verboseLog(STRINGS.colorBroadcast, currentColor, channelId);
-  request(
-    `https://api.twitch.tv/extensions/message/${channelId}`,
-    {
-      method: "POST",
-      headers,
-      body,
-    },
-    (err, res) => {
-      if (err) {
-        console.log(STRINGS.messageSendError, channelId, err);
-      } else {
-        verboseLog(STRINGS.pubsubResponse, channelId, res.statusCode);
-      }
+    let url = "";
+    switch (config.mode) {
+      case "dpp":
+        // Dofus page persos
+        url = "https://www.dofus.com/fr/mmorpg/communaute/annuaires/pages-persos/" + config.link + "/caracteristiques";
+        break;
+      case "dr":
+        // Dofus Room
+        url = "https://www.dofusroom.com/buildroom/build/show/" + config.link;
+        break;
     }
-  );
+
+    return new Promise((resolve, rejection) => {
+      try {
+        if (config.mode == "dr") {
+          (async () => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url);
+            await new Promise((r) => setTimeout(r, 1000));
+            htmlResponse = await page.content();
+            await browser.close();
+            resolve(htmlResponse);
+          })();
+        } else if (config.mode == "dpp") {
+          var options = {
+            method: "GET",
+            url: url,
+          };
+          cloudscraper(options).then(function (htmlString) {
+            resolve(htmlString);
+          });
+        }
+      } catch (err) {
+        rejection(err);
+      }
+    });
+  }
 }
+
+// function attemptStuffBroadcast(channelId) {
+//   // Check the cool-down to determine if it's okay to send now.
+//   const now = Date.now();
+//   const cooldown = channelCooldowns[channelId];
+//   if (!cooldown || cooldown.time < now) {
+//     // It is.
+//     sendStuffBroadcast(channelId);
+//     channelCooldowns[channelId] = { time: now + channelCooldownMs };
+//   } else if (!cooldown.trigger) {
+//     // It isn't; schedule a delayed broadcast if we haven't already done so.
+//     cooldown.trigger = setTimeout(sendStuffBroadcast, now - cooldown.time, channelId);
+//   }
+// }
+
+// function sendStuffBroadcast(channelId) {
+//   // Set the HTTP headers required by the Twitch API.
+//   const headers = {
+//     "Client-ID": clientId,
+//     "Content-Type": "application/json",
+//     Authorization: bearerPrefix + makeServerToken(channelId),
+//   };
+
+//   // Create the POST body for the Twitch API request.
+//   // const currentColor = color(channelColors[channelId] || initialColor).hex();
+//   const body = JSON.stringify({
+//     content_type: "text/html",
+//     message: htmlResponse,
+//     targets: ["broadcast"],
+//   });
+
+//   // Send the broadcast request to the Twitch API.
+//   // verboseLog(STRINGS.colorBroadcast, currentColor, channelId);
+//   request(
+//     `https://api.twitch.tv/extensions/message/${channelId}`,
+//     {
+//       method: "POST",
+//       headers,
+//       body,
+//     },
+//     (err, res) => {
+//       if (err) {
+//         console.log(STRINGS.messageSendError, channelId, err);
+//       } else {
+//         verboseLog(STRINGS.pubsubResponse, channelId, res.statusCode);
+//       }
+//     }
+//   );
+// }
+
+// function sendColorBroadcast(channelId) {
+//   // Set the HTTP headers required by the Twitch API.
+//   const headers = {
+//     "Client-ID": clientId,
+//     "Content-Type": "application/json",
+//     Authorization: bearerPrefix + makeServerToken(channelId),
+//   };
+
+//   // Create the POST body for the Twitch API request.
+//   const currentColor = color(channelColors[channelId] || initialColor).hex();
+//   const body = JSON.stringify({
+//     content_type: "application/json",
+//     message: currentColor,
+//     targets: ["broadcast"],
+//   });
+
+//   // Send the broadcast request to the Twitch API.
+//   verboseLog(STRINGS.colorBroadcast, currentColor, channelId);
+//   request(
+//     `https://api.twitch.tv/extensions/message/${channelId}`,
+//     {
+//       method: "POST",
+//       headers,
+//       body,
+//     },
+//     (err, res) => {
+//       if (err) {
+//         console.log(STRINGS.messageSendError, channelId, err);
+//       } else {
+//         verboseLog(STRINGS.pubsubResponse, channelId, res.statusCode);
+//       }
+//     }
+//   );
+// }
 
 // Create and return a JWT for use by this service.
-function makeServerToken(channelId) {
-  const payload = {
-    exp: Math.floor(Date.now() / 1000) + serverTokenDurationSec,
-    channel_id: channelId,
-    user_id: ownerId, // extension owner ID for the call to Twitch PubSub
-    role: "external",
-    pubsub_perms: {
-      send: ["*"],
-    },
-  };
-  return jsonwebtoken.sign(payload, secret, { algorithm: "HS256" });
-}
+// function makeServerToken(channelId) {
+//   const payload = {
+//     exp: Math.floor(Date.now() / 1000) + serverTokenDurationSec,
+//     channel_id: channelId,
+//     user_id: ownerId, // extension owner ID for the call to Twitch PubSub
+//     role: "external",
+//     pubsub_perms: {
+//       send: ["*"],
+//     },
+//   };
+//   return jsonwebtoken.sign(payload, secret, { algorithm: "HS256" });
+// }
 
-function userIsInCooldown(opaqueUserId) {
-  // Check if the user is in cool-down.
-  const cooldown = userCooldowns[opaqueUserId];
-  const now = Date.now();
-  if (cooldown && cooldown > now) {
-    return true;
-  }
+// function userIsInCooldown(opaqueUserId) {
+//   // Check if the user is in cool-down.
+//   const cooldown = userCooldowns[opaqueUserId];
+//   const now = Date.now();
+//   if (cooldown && cooldown > now) {
+//     return true;
+//   }
 
-  // Voting extensions must also track per-user votes to prevent skew.
-  userCooldowns[opaqueUserId] = now + userCooldownMs;
-  return false;
-}
+//   // Voting extensions must also track per-user votes to prevent skew.
+//   userCooldowns[opaqueUserId] = now + userCooldownMs;
+//   return false;
+// }
